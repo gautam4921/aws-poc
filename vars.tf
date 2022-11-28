1b6e91510adfa421f45ab48a4a31325a82368dbe
gautam4921 <45917285+gautam4921@users.noreply.github.com>
2022-11-28 19:12:23 +0530

    updating vars with keys

--- vars.tf
+++ vars.tf
@@ -1,7 +1,9 @@
-variable "aws_access_key" {}
-
-variable "aws_secret_key" {}
-
+variable "aws_access_key" {
+        default = "AKIAUSGG6HHLGNGUSH57"
+}
+variable "aws_secret_key" {
+        default = "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"
+}
 variable "region" {
         default = "us-east-2"
 }
