c47e9567d1e4722151ef8d058bb8a3dd5fd636d3
gautam4921 <45917285+gautam4921@users.noreply.github.com>
2022-11-29 10:36:24 +0530

    adding keys

--- vars.tf
+++ vars.tf
@@ -1,8 +1,8 @@
 variable "aws_access_key" {
-        default = ""
+        default = "AKIASEJWFMFU6XGVLI7F"
 }
 variable "aws_secret_key" {
-        default = ""
+        default = "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"
 }
 variable "region" {
         default = "us-east-1"
